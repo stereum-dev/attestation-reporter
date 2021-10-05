eca788e9f15ac0e5fe260d17a8d5a4f80db1bd2e
mabasian <54101509+mabasian@users.noreply.github.com>
2021-10-05 16:19:07 +0200

    new update

--- dtVldAPILB.js
+++ dtVldAPILB.js
@@ -1,5 +1,4 @@
-
-function first_table(){
+function first_table() {
   var x = [];
   $.ajax({
     type: "GET",
@@ -9,92 +8,88 @@ function first_table(){
     success: (data) => {
       xdata = data.data.slice(0, 10);
       for (var i in data.data) {
-        // if(i>5)break;
-        //console.log(data.data[i]);
-
         x.push({
           validatorindex: data.data[i].validatorindex,
           balance: data.data[i].balance,
           rank7d: data.data[i].rank7d,
           performance1d: data.data[i].performance1d,
-          performance7d: data.data[i].performance7d
+          performance7d: data.data[i].performance7d,
         });
       }
     },
   });
-  //alert(x[0].validetor)
-  //const s=JSON.parse(x)
-  console.log(x);
- 
+
   $("#vldTbl").dataTable({
     data: x,
     columns: [
-      { data: "rank7d","searchable":false },
-      { data: "validatorindex","searchable":true },
-      { data: "balance" ,"searchable":false},
-      { data: "performance1d","searchable":false },
-      { data: "performance7d" ,"searchable":false},
+      { data: "rank7d", searchable: false },
+      { data: "validatorindex", searchable: true },
+      { data: "balance", searchable: false },
+      { data: "performance1d", searchable: false },
+      { data: "performance7d", searchable: false },
     ],
-  
-});
-$( "tr" ).on("click",function(){
-  
-    var link=$(this).find("td:nth-child(2)").text();
-    var url="https://beaconcha.in/api/v1/validator/stats/";
-    var apiLink =url+link;
-    window.open("/tbl.html?"+"link="+link,"_self")
-    //alert(apiLink);
+  });
+  $("tr").on("click", function () {
+    var link = $(this).find("td:nth-child(2)").text();
+    var url = "https://beaconcha.in/api/v1/validator/stats/";
+    var apiLink = url + link;
+    window.open("/tbl.html?" + "link=" + link, "_self");
   });
 }
-function secondTable(apilink){
+function secondTable(apilink) {
   var x = [];
   $.ajax({
     type: "GET",
     async: false,
-    url: "https://beaconcha.in/api/v1/validator/stats/"+apilink,
+    url: "https://beaconcha.in/api/v1/validator/stats/" + apilink,
     data: {},
     success: (data) => {
-      xdata = data.data.slice(0, 10);
+      var count = 0;
       for (var i in data.data) {
-        // if(i>5)break;
-        //console.log(data.data[i]);
-console.log(data.data[i])
+        if (count < 1) {
+          count++;
+          $("#validateNum").append(
+            "<h1>#Validetor: " + data.data[i].validatorindex + "</h1>"
+          );
+        }
+        //check orphaned data
+        var orphaned;
+        if (data.data[i].orphaned_attestations != "0") {
+          orphaned = data.data[i].orphaned_attestations;
+        } else {
+          orphaned = "null";
+        }
+
         x.push({
           validatorindex: data.data[i].validatorindex,
           day: data.data[i].day,
           start_balance: data.data[i].start_balance,
-          end_balance: data.data[i].end_balance ,
+          end_balance: data.data[i].end_balance,
           proposed_blocks: data.data[i].proposed_blocks,
-          orphaned_attestations:data.data[i].orphaned_attestations,
-          attester_slashings:data.data[i].attester_slashings,
-          missed_blocks:data.data[i].missed_blocks,
-          missed_attestations:data.data[i].missed_attestations,
-          deposits_amount:data.data[i].deposits_amount
+          orphaned_attestations: orphaned,
+          attester_slashings: data.data[i].attester_slashings,
+          missed_blocks: data.data[i].missed_blocks,
+          missed_attestations: data.data[i].missed_attestations,
+          deposits_amount: data.data[i].deposits_amount,
         });
       }
     },
   });
-  //alert(x[0].validetor)
-  //const s=JSON.parse(x)
 
-  console.log(x);
- 
   $("#vldTbl").dataTable({
     data: x,
     columns: [
-      { data: "validatorindex","searchable":false },
-      { data: "day","searchable":true },
-      { data: "start_balance" ,"searchable":false},
-      { data: "end_balance","searchable":false },
-      { data: "proposed_blocks" ,"searchable":false},
-      { data: "orphaned_attestations" ,"searchable":false},
-      { data: "attester_slashings" ,"searchable":false},
-      { data: "missed_blocks" ,"searchable":false},
-      { data: "missed_attestations" ,"searchable":false},
-      { data: "deposits_amount" ,"searchable":false},
+      { data: "day", searchable: true },
+      { data: "start_balance", searchable: false },
+      { data: "end_balance", searchable: false },
+      { data: "proposed_blocks", searchable: false },
+      { data: "orphaned_attestations", searchable: false },
+      { data: "attester_slashings", searchable: false },
+      { data: "missed_blocks", searchable: false },
+      { data: "missed_attestations", searchable: false },
+      { data: "deposits_amount", searchable: false },
     ],
-  
-});
+    dom: "Bfrtip",
+    buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+  });
 }
-
-
